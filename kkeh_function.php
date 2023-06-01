505b0a7f2cd0b10d248393a52c9d079174096662
atarashikazuma <atarashi.kazuma@viva-eureka.co.jp>
2023-06-01 17:49:47 +0900

    kkeh_function 修正版

--- kkeh_function.php
+++ kkeh_function.php
@@ -64,7 +64,7 @@
      */
     function connect(): PDO
     {
-        $pdo = new PDO('mysql:host=localhost; dbname=honkaku; charset=utf8mb4', 'root', '');
+        $pdo = new PDO('mysql:host=localhost; dbname=employee; charset=utf8mb4', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         return $pdo;
